8f2500d898e76c6381c9175369581eaac083157a
Lautaro Cometti <lautic003@gmail.com>
2023-02-16 09:35:45 -0300

    created relationships between tables

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Recipe } = sequelize.models;
+const { Recipe, Diet } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+Recipe.belongsToMany(Diet, { through: "Recipe_Diet" })
+Diet.belongsToMany(Recipe, { through: "Recipe_Diet" })
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
