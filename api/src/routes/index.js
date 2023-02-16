3be742112a88a3bf06ec3ed2a043953c56d49e8d
Lautaro Cometti <lautic003@gmail.com>
2023-02-16 09:49:48 -0300

    established routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const recipesRouter = require('./recipesRoutes')
+const dietsRouter = require('./dietsRoutes')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+router.use('/recipes', recipesRouter)
+router.use('/diets', dietsRouter)
 
 module.exports = router;
---
+++ api/src/routes/recipesRoutes.js
@@ -0,0 +1,7 @@
+const { Router } = require('express')
+
+const recipesRouter = Router()
+
+
+
+module.exports = recipesRouter
