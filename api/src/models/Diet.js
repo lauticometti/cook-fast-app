bfb589000cac7bf05b0753c0f2117d875a7999e3
Lautaro Cometti <lautic003@gmail.com>
2023-02-16 08:59:19 -0300

    created sequelize models Recipe and Diet

---
+++ api/src/models/Diet.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Diet', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  },
+  {
+    timestamps: false
+  }
+  );
+};
---
+++ api/src/models/Recipe.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Recipe', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    healthScore: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    steps: {
+      type: DataTypes.ARRAY(DataTypes.JSONB),
+      allowNull: false
+    }
+  },
+  {
+    timestamps: false
+  }
+  );
+};
