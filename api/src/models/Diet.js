856a494e4a596297b1632d314a12b467ad80a88d
Lautaro Cometti <lautic003@gmail.com>
2023-02-16 22:52:25 -0300

    changed datatypes of id property. before uuid, now just id.

--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -5,8 +5,8 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Diet', {
     id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV1,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
       primaryKey: true
     },
     name: {
--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -5,8 +5,8 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Recipe', {
     id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV1,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
       primaryKey: true
     },
     name: {
