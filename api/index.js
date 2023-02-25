8c933f17834b7db0e8a394dadb8a69fa67136ea1
Lautaro Cometti <lautic003@gmail.com>
2023-02-25 08:40:33 -0300

    Now, when server is turn on, the diets are charged on the database

--- api/index.js
+++ api/index.js
@@ -19,12 +19,16 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const getDiets = require('./src/controllers/diets');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
-  
   });
+}).then(() => {
+  getDiets()
+}).catch((err) => {
+  console.log(err);
+})
 
-});
